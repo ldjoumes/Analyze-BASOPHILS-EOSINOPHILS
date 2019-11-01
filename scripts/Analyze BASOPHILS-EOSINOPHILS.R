db64cbf7cde9c508136688ec1520e9f8ea15107f
Loic Stephane Djoumessi <56736995+ldjoumes@users.noreply.github.com>
2019-11-01 15:44:25 -0400

    Update Analyze BASOPHILS-EOSINOPHILS.R

--- scripts/Analyze BASOPHILS-EOSINOPHILS.R
+++ scripts/Analyze BASOPHILS-EOSINOPHILS.R
@@ -2,24 +2,25 @@
 
 install.packages("ggplot2")
 library(ggplot2)
-
-install.packages("scatterplot3d")
-library(scatterplot3d)
-
 ## Read data
 IBS1 <- read.csv("RobinsonEtAl_Sup1.csv", header = TRUE)
-
-##  Single Regressions for BMI vs. each blood data variable
+head(IBS1)
+##  Single Regressions for BMI vs. Basophils
 ##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
 ##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
 ##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
 ##  http://r-statistics.co/Linear-Regression.html
-
-
+##  Single Regressions for BMI vs. Eosinophils
+##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
+##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
+##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
+##  http://r-statistics.co/Linear-Regression.html
 single.regression <- lm(BMI ~ SerumCortisol, data=IBS1)
 summary(single.regression)
 
+summary(single.regression)
 single.regression <- lm(BMI ~ CRP, data=IBS1)
+
 summary(single.regression)
 
 single.regression <- lm(BMI ~ ESR, data=IBS1)
@@ -33,4 +34,34 @@ summary(single.regression)
 
 single.regression <- lm(BMI ~ Lymphocytes, data=IBS1)
 summary(single.regression)
+single.regression <- lm(BMI ~ Basophils, data=IBS1)
+print(single.regression)
+
+
+install.packages("car")
+library(car)
+install.packages("cardata")
+library(carData)
+
+data <- read.csv('RobinsonEtAl_Sup1.csv')
+df<-na.omit(data)
+
+#BMI, Basophils
+single.regression <- lm(BMI ~ Basophils, data=IBS1)
+print(single.regression)
+
+ggplot(df, aes(x=BMI, y=Basophils)) +
+  geom_point() +    
+  geom_smooth(method=lm)
+df<-na.omit(data)
+#BMI, Eosinophils
+single.regression <-lm(BMI ~ Eosinophils, data =IBS1)
+print(single.regression)
+ggplot(df,aes(x=BMI, y=Eosinophils)) +
+geom_point() +
+geom_smooth(method=lm)
+
+
+  
+
 
