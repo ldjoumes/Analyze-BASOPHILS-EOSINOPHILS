449f8531030f50494d9a0304278ecfe4f54551cd
Loic Stephane Djoumessi <56736995+ldjoumes@users.noreply.github.com>
2019-11-07 11:54:37 -0500

    Update Analyze BASOPHILS-EOSINOPHILS.R

--- scripts/Analyze BASOPHILS-EOSINOPHILS.R
+++ scripts/Analyze BASOPHILS-EOSINOPHILS.R
@@ -19,21 +19,6 @@ df<-na.omit(data)
 ggplot(df, aes(x=BMI, y=Basophils)) +
   geom_point() +    
   geom_smooth(method=lm)
-
-##  Single Regressions for BMI vs. Eosinophils
-##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
-##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
-##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
-##  http://r-statistics.co/Linear-Regression.html
-
-#BMI, Eosinophils
-single.regression <- lm(BMI ~ Eosinophils, data=IBS1)
-print(single.regression)
-df<-na.omit(data)
-ggplot(df,aes(x=BMI, y=Eosinophils)) +
-geom_point() +
-geom_smooth(method=lm)
-
 sink("data_output/basophils_regression.txt")
 print(single.regression)
 sink() 
@@ -41,7 +26,6 @@ sink()
 ggplot(IBS1, aes(x=BMI, y=Basophils)) +
   geom_point()+
   geom_smooth(method= lm)
-
 
 png("fig_output/Basophils_scatterplot.png")
 Basophils_scatterplot <- ggplot(IBS1, aes(x = BMI, y = Basophils)) +
@@ -63,6 +47,20 @@ print(Basophils_boxplot)
 dev.off()
 
 
+##  Single Regressions for BMI vs. Eosinophils
+##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
+##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
+##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
+##  http://r-statistics.co/Linear-Regression.html
+
+#BMI, Eosinophils
+single.regression <- lm(BMI ~ Eosinophils, data=IBS1)
+print(single.regression)
+df<-na.omit(data)
+ggplot(df,aes(x=BMI, y=Eosinophils)) +
+geom_point() +
+geom_smooth(method=lm)
+
 sink("data_output/Eosinophils_regression.txt")
 print(single.regression)
 sink() 
@@ -71,25 +69,21 @@ ggplot(IBS1, aes(x=BMI, y=Eosinophils)) +
   geom_point()+
   geom_smooth(method= lm)
 
-
-
 png("fig_output/Eosinophlis_scatterplot.png")
 Eosinophils_scatterplot <- ggplot(IBS1, aes(x = BMI, y = Eosinophils)) +
   geom_point() +    
   geom_smooth(method = lm) 
-
-print(Basophils_scatterplot)
+print(Esoinophils_scatterplot)
 dev.off()
 
 boxplot(Eosinophils ~ IBS.subtype, data = IBS1, main="Eosinophils by IBS1 subtype", 
         xlab = "IBS1.subtype", ylab = "Eosinophils"
 )
-
 png("fig_output/Eosinophils_boxplot.png")
 Eosinophils_boxplot <- boxplot(Eosinophils ~ IBS.subtype, data = IBS1, main="Eosinophils by IBS1 subtype", 
                              xlab = "IBS1.subtype", ylab = "Eosinophils"
 )
-print(Basophils_boxplot)
+print(Eosinophils_boxplot)
 dev.off()
 
 
