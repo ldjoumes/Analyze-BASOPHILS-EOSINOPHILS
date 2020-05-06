4b21c62fadfb27982b49426c0518ebcdb3a9e45f
Loic Stephane Djoumessi <56736995+ldjoumes@users.noreply.github.com>
2020-05-06 13:20:46 -0400

    Update Analyze BASOPHILS-EOSINOPHILS.R

--- scripts/Analyze BASOPHILS-EOSINOPHILS.R
+++ scripts/Analyze BASOPHILS-EOSINOPHILS.R
@@ -103,42 +103,4 @@ print(Eosinophils.aov)
 sink()
 
 
-### BTEC395 S2020  Final Project2 ( Additinal lines of codes)
-##  By Stephane Djoumessi
-## Set the working directory to setwd("//130.85.49.17/Profiles$/fq65257/Desktop/Analyze-BASOPHILS-EOSINOPHILS-master/scripts") 
-setwd("./")
-
-## Read data
-IBS <- read.csv("../data/GSE124549_20200330.csv", header = TRUE)
-
-head(IBS)
-
-## Recursive analysis for regression  - RNA Expression
-## https://stackoverflow.com/questions/42464767/how-to-run-lm-regression-for-every-column-in-r
-## https://stackoverflow.com/questions/44170937/performing-lm-and-segmented-on-multiple-columns-in-r
-## http://www.learnbymarketing.com/tutorials/explaining-the-lm-summary-in-r/
-## https://tutorials.iq.harvard.edu/R/Rstatistics/Rstatistics.html
-
-
-## Access only the columns with RNA Expression (from column 37-286)
-names(IBS)[37:286]
-
-##  Make a data frame of list type
-storage <- list()
-
-## linear regression for each expressed gene
-for(i in names(IBS)[37:286]){
-  storage[[i]]  <- lm(get(i) ~ Basophils..x10.9.cells.L., IBS)
-}
-
-## Linear regression resulst from 01 gene present in storage list
-summary(storage$AGO2)
-summary(storage$AGO2)$r.squared
-summary(storage$AGO2)$coefficients[,4]
-
-## output the results of the 250 genes vs Basophils, in data_output folder
-sink('../data_output/Basophils_storage.txt', append = TRUE)
-print(storage)
-sink()
-
 
